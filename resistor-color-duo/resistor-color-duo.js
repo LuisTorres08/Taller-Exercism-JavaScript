c89d9c3cac95a9792fb43148c927156a4de52301
Luis Torres <Luis@Torres.com>
2021-05-24 13:39:43 -0500

    add resistor color duo

---
+++ resistor-color-duo/resistor-color-duo.js
@@ -0,0 +1,20 @@
+//
+// This is only a SKELETON file for the 'Resistor Color Duo' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const decodedValue = ([firstColor, secondColor]) => (COLORS.indexOf(firstColor.toLowerCase()) * 10) + COLORS.indexOf(secondColor.toLowerCase());
+
+export const COLORS = [
+  'black',
+  'brown',
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'blue',
+  'violet',
+  'grey',
+  'white',
+];
+  
