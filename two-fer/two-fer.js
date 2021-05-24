f0503da5e905f5fea6621134e79756823f8328ff
Luis Torres <Luis@Torres.com>
2021-05-24 12:57:01 -0500

    two-fer upgrade

--- two-fer/two-fer.js
+++ two-fer/two-fer.js
@@ -3,6 +3,6 @@
 // convenience to get you started writing code faster.
 //
 
-export const twoFer = () => {
-  throw new Error('Remove this statement and implement this function');
+export const twoFer = (nombre = "you") => {
+  return `One for ${nombre}, one for me.`;
 };
