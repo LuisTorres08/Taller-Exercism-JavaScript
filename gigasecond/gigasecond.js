5632526bcefefeba4c1adb1e0ba292055360e789
Luis Torres <Luis@Torres.com>
2021-05-24 14:08:54 -0500

    add gigasecond

---
+++ gigasecond/gigasecond.js
@@ -0,0 +1,10 @@
+//
+// This is only a SKELETON file for the 'Gigasecond' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+const gigasecondToMillisecond = 10e11;
+
+export const gigasecond = (date) => 
+    new Date(date.getTime() + gigasecondToMillisecond);
+
